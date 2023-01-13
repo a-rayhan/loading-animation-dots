ecd7d34e616201909bf7e60679e2863519f5c0b6
rayhan <aburayhandh@gmail.com>
2023-01-13 20:16:25 +0600

    Loading dots js functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 /*=============== ANIMATED DOTS JS ===============*/
+const dots = document.querySelector('.loading__dots')
+
+const addAnimate = () => {
+    //Add animate class
+    dots.classList.add('animate')
+
+    setTimeout(() => {
+        // Rmove animate class
+        dots.classList.remove('animate')
+
+        setTimeout(() => {
+            // Run the function again
+            addAnimate()
+        }, 100)
+    }, 2600)
+}
+
+addAnimate();
